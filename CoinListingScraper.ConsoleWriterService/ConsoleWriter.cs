a011b5fd8d13b1d8ded7fc1742df681739002d5d
Chris <chrispauldillon@live.com>
2021-11-23 15:51:46 +0000

    Added console helper class to easily colour text, reenabled coinbase polling as a test

---
+++ CoinListingScraper.ConsoleWriterService/ConsoleWriter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CoinListingScraper.ConsoleWriterService
+{
+    public static class ConsoleWriter
+    {
+        private static ConsoleColor _defaultColour = ConsoleColor.Cyan;
+
+        public static void WriteLine(string text)
+        {
+            Console.ForegroundColor = _defaultColour;
+            Console.WriteLine(text);
+        }
+
+        public static void WriteLinePositive(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+            Console.ForegroundColor = _defaultColour;
+        }
+
+        public static void WriteLineNegative(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(text);
+            Console.ForegroundColor = _defaultColour;
+        }
+    }
+}
--- CoinListingScraper.DiscordAnnouncer/Program.cs
+++ CoinListingScraper.DiscordAnnouncer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
+using CoinListingScraper.ConsoleWriterService;
 using CoinListingScraper.DiscordAnnouncer.Models;
 using CoinListingScraper.GateIOService;
 using CoinListingScraper.KuCoinService;
@@ -19,6 +20,8 @@ namespace CoinListingScraper.DiscordAnnouncer
     {
         private readonly ExchangeActionConfig _kuCoinConfig = new ExchangeActionConfig("KuCoin");
         private readonly ExchangeActionConfig _binanceConfig = new ExchangeActionConfig("Binance");
+        private readonly ExchangeActionConfig _coinbaseConfig = new ExchangeActionConfig("Coinbase");
+
         private static IDictionary<string, CoinListing> coinListings = new Dictionary<string, CoinListing>(); //Store a collection of coinListings in memory to prevent additional write operations
 
         private DiscordHelper _discordService;
@@ -34,8 +37,8 @@ namespace CoinListingScraper.DiscordAnnouncer
 
         public async Task Startup()
         {
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.WriteLine("Loading up...");
+            
+            ConsoleWriter.WriteLine("Loading up...");
             coinListings = JsonHelper.LoadPreviouslyFoundCoins();
 
             var builder = new ConfigurationBuilder()
@@ -59,11 +62,11 @@ namespace CoinListingScraper.DiscordAnnouncer
             _discordService = new DiscordHelper();
             await _discordService.StartBotAsync();
 
-            Console.WriteLine("Discord Bot now online");
-            Console.WriteLine("Polling every 5 seconds");
+            ConsoleWriter.WriteLine("Discord Bot now online");
+            ConsoleWriter.WriteLine("Polling every 5 seconds");
 
             //var timer = new Timer(TimeSpan.FromMinutes(05).TotalMilliseconds);
-            var timer = new Timer(TimeSpan.FromMilliseconds(5 * 1000).TotalMilliseconds);
+            var timer = new Timer(TimeSpan.FromMilliseconds(30 * 1000).TotalMilliseconds);
             timer.AutoReset = true;
             timer.Elapsed += TimerProc;
             timer.Start();
@@ -74,10 +77,10 @@ namespace CoinListingScraper.DiscordAnnouncer
         private async Task BuyAndSellCrypto(string ticker, ExchangeActionConfig config) //new coin has been found, execute buy/sell
         {
             isAlreadyBuying = true;
-            Console.WriteLine("Attempting to Place Order...");
+            ConsoleWriter.WriteLine($"Attempting to Place Order for {ticker} at {config.ExchangeName}...");
             var orderResult = await _gateService.PlaceOrder(ticker);
 
-            Console.WriteLine("Order Successfully Placed at " + DateTime.UtcNow);
+            ConsoleWriter.WriteLine("Order Successfully Placed at " + DateTime.UtcNow);
             Thread.Sleep(30000); //sleep an additional 30 seconds to ensure order has been filled before proceeding
 
             if (config.AutomatedSell)
@@ -85,17 +88,17 @@ namespace CoinListingScraper.DiscordAnnouncer
                 for (var i = 0; i < 30; i++)
                 {
                     Thread.Sleep(30000);
-                    Console.WriteLine("Checking last price...");
+                    ConsoleWriter.WriteLine("Checking last price...");
 
                     var didSell = await _gateService.AttemptSellOrder(ticker, Convert.ToDouble(orderResult.Amount), Convert.ToDouble(orderResult.Price));
                     if (!didSell) continue;
 
-                    Console.WriteLine("Successfully Sold Order at " + DateTime.UtcNow);
+                    ConsoleWriter.WriteLine("Successfully Sold Order at " + DateTime.UtcNow);
                     break;
                 }
             }
    
-            Console.WriteLine("Continuing to poll...");
+            ConsoleWriter.WriteLine("Continuing to poll...");
             isAlreadyBuying = false;
         }
 
@@ -107,12 +110,12 @@ namespace CoinListingScraper.DiscordAnnouncer
 
                 await PollKuCoinApi();
                 await PollBinanceApi(); 
-                //await PollCoinBaseApi();
+                await PollCoinBaseApi();
 
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                ConsoleWriter.WriteLine(ex.Message);
             }
 
         }
@@ -136,9 +139,7 @@ namespace CoinListingScraper.DiscordAnnouncer
 
             var msg = $"KuCoin will list {coinListing.Name} ({coinListing.Ticker})";
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(msg + " at {DateTime.UtcNow}!");
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            ConsoleWriter.WriteLine(msg + " at {DateTime.UtcNow}!");
 
             _discordService.Announce(msg); //There is no need for buying/selling to wait for the discord action
 
@@ -163,9 +164,8 @@ namespace CoinListingScraper.DiscordAnnouncer
             JsonHelper.WriteCoinToJsonFile(coinListings);
 
             var msg = coinListing?.Ticker == null ? $"Binance will list {coinListing.Name}" : $"Binance will list {coinListing.Name} ({coinListing.Ticker})";
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(msg + $" at {DateTime.UtcNow}!");
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            ConsoleWriter.WriteLine(msg + $" at {DateTime.UtcNow}!");
 
             _discordService.Announce(msg); //There is no need for buying/selling to wait for the discord action
 
@@ -174,7 +174,7 @@ namespace CoinListingScraper.DiscordAnnouncer
 
         private async Task PollCoinBaseApi()
         {
-            Console.WriteLine("Polling CoinBase API...");
+            ConsoleWriter.WriteLine("Polling CoinBase API...");
             var coinListingList = await _scraperService.GetLatestCoinBaseArticle();
 
             foreach (var listing in coinListingList)
@@ -190,14 +190,11 @@ namespace CoinListingScraper.DiscordAnnouncer
                 JsonHelper.WriteCoinToJsonFile(coinListings);
 
                 var msg = $"CoinBase will list {coinListing.Ticker}";
+                ConsoleWriter.WriteLine(msg + $" at {DateTime.UtcNow}!");
 
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(msg + $" at {DateTime.UtcNow}!");
-                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                _discordService.Announce(msg); //There is no need for buying/selling to wait for the discord action
 
-                await _discordService.Announce(msg); //There is no need for buying/selling to wait for the discord action
-
-                //await BuyAndSellCrypto(coinListing.Ticker);
+                await BuyAndSellCrypto(coinListing.Ticker, _coinbaseConfig);
             }
         }
     }
